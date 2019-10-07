f11aed6fffb46884cd8cb9d5f19c9b1f41378627
Austin Hedeen <austin.hedeen@gmail.com>
2019-10-07 11:11:23 -0700

    Implement back-end API server

---
+++ back-end/src/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(routes);
+
+module.exports = {
+  server: app,
+  start: (port) => {
+    app.listen(port, () => console.log(`Server started on port ${port}`));
+  },
+};
